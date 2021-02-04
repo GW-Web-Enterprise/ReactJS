cde056cd3d30a98886cadab6f1c710cc1f89f56e
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-02-04 12:16:42 +0700

    ➕ update(constants): add regex validations

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const REGEX_EMAIL = /^(([^<>()\[\]\\.,;:\s-@#$!%^&*+=_/`?{}|'"]+(\.[^<>()\[\]\\.,;:\s-@_!#$%^&*()=+/`?{}|'"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+export const REGEX_NO_SPACES = /^\S*$/
+export const REGEX_NO_LEADING_TRAILING_DOUBLE_SPACES = /^[a-zA-Z0-9]+(?: [a-zA-Z0-9]+)*$/
