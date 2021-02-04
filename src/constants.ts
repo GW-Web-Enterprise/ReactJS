dff01de1ed62eb8fe6bc0d4746234dad830faed2
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-02-04 18:25:57 +0700

    ➕ update(constants): add fullname validation error

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,15 @@ import { RegisterOptions } from "react-hook-form";
 export const REGEX_EMAIL = /^(([^<>()\[\]\\.,;:\s-@#$!%^&*+=_/`?{}|'"]+(\.[^<>()\[\]\\.,;:\s-@_!#$%^&*()=+/`?{}|'"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
 export const REGEX_NO_SPACES = /^\S*$/
 export const REGEX_NO_LEADING_TRAILING_DOUBLE_SPACES = /^[a-zA-Z0-9]+(?: [a-zA-Z0-9]+)*$/
+export const REGEX_FULLNAME = /^([a-zA-Z]+\s)*[a-zA-Z]+$/
+
+const fullnameErr = 'Your fullname must contain between 4 and 64 characters'
+export const FULLNAME_INP_ERR: RegisterOptions = {
+    required: fullnameErr,
+    minLength: { value: 4, message: fullnameErr },
+    maxLength: { value: 64, message: fullnameErr },
+    pattern: { value: REGEX_FULLNAME, message: 'Please enter a valid fullname (alphabet characters only, no space at both end, and no consecutive spaces)' }
+}
 
 const emailErr = 'Please enter a valid email';
 export const EMAIL_INP_ERR: RegisterOptions = {
