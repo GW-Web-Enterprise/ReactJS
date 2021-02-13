f30e798b8fe6e08e87b2c99ac6566dc4f92e8722
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-02-13 11:15:31 +0700

    💄 update(faculty page)

--- src/Components/TabNav.tsx
+++ src/Components/TabNav.tsx
@@ -11,12 +11,20 @@ import {
     ListItemSecondaryAction,
     ListItemText,
     Tooltip,
-    Paper
+    Paper,
+    FormControl,
+    InputLabel,
+    NativeSelect,
+    FormHelperText,
+    Snackbar,
+    Button
 } from '@material-ui/core';
 import React, { Fragment, useState, VFC } from 'react';
 import { SearchBar } from './SearchBar';
 import { AddCircle, RemoveCircle } from '@material-ui/icons';
 import { cloneComponent } from '../utils';
+import { PopoverItem } from './PopoverItem';
+import { Alert } from '@material-ui/lab';
 
 type TabPanelProps = { children?: React.ReactNode; index: number; value: number };
 
@@ -25,8 +33,8 @@ const TabPanel: VFC<TabPanelProps> = ({ children, value, index, ...other }) => {
         <div
             role="tabpanel"
             hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
+            id={`tabpanel-${index}`}
+            aria-labelledby={`tab-${index}`}
             {...other}
         >
             {value === index && <Box p={1}>{children}</Box>}
@@ -36,9 +44,20 @@ const TabPanel: VFC<TabPanelProps> = ({ children, value, index, ...other }) => {
 
 export const TabNav: VFC = () => {
     const [value, setValue] = useState(0);
+    const [open, setOpen] = useState(false);
     const handleChange = (e: React.ChangeEvent<{}>, newValue: number) => setValue(newValue);
     return (
         <Fragment>
+            <Snackbar
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                open={open}
+                autoHideDuration={6000}
+                onClose={() => setOpen(false)}
+            >
+                <Alert onClose={() => setOpen(false)} severity="success">
+                    Member removed (this notification should be closed before doing anything else)
+                </Alert>
+            </Snackbar>
             <Paper square>
                 <Tabs
                     value={value}
@@ -65,8 +84,8 @@ export const TabNav: VFC = () => {
                             </ListItemAvatar>
                             <ListItemText primary="Steve Jobs" secondary="Student" />
                             <ListItemSecondaryAction>
-                                <Tooltip title="Remove this member from the faculty">
-                                    <IconButton edge="end" aria-label="delete">
+                                <Tooltip title="Remove this member from the faculty" arrow>
+                                    <IconButton edge="end" aria-label="delete" onClick={() => setOpen(true)}>
                                         <RemoveCircle />
                                     </IconButton>
                                 </Tooltip>
@@ -78,7 +97,7 @@ export const TabNav: VFC = () => {
             <TabPanel value={value} index={1}>
                 <List style={{ position: 'relative', overflow: 'auto', maxHeight: 300 }}>
                     <Typography color="primary" style={{ paddingLeft: '16px' }}>
-                        Total: 20 users
+                        Total: 20 users NOT in this faculty
                     </Typography>
                     <SearchBar placeholder="Search users..." />
                     {cloneComponent(20)(
@@ -88,11 +107,36 @@ export const TabNav: VFC = () => {
                             </ListItemAvatar>
                             <ListItemText primary="Steve Jobs" />
                             <ListItemSecondaryAction>
-                                <Tooltip title="Add this user to the faculty">
-                                    <IconButton edge="end" aria-label="delete">
-                                        <AddCircle />
-                                    </IconButton>
-                                </Tooltip>
+                                <PopoverItem
+                                    placement="top"
+                                    renderToggle={(toggle, toggleEl) => (
+                                        <Tooltip title="Add this user to the faculty" arrow>
+                                            <IconButton edge="end" aria-label="delete" onClick={toggle} ref={toggleEl}>
+                                                <AddCircle />
+                                            </IconButton>
+                                        </Tooltip>
+                                    )}
+                                    renderPopContent={() => (
+                                        <FormControl>
+                                            <InputLabel htmlFor="age-native-helper">Faculty role</InputLabel>
+                                            <NativeSelect
+                                                value="None"
+                                                inputProps={{
+                                                    name: 'role',
+                                                    id: 'faculty-role-native-helper'
+                                                }}
+                                            >
+                                                <option aria-label="None" value="" />
+                                                <option value={10}>student</option>
+                                                <option value={20}>coordinicator</option>
+                                            </NativeSelect>
+                                            <FormHelperText>What this member will be allowed to do?</FormHelperText>
+                                            <Button variant="contained" color="primary">
+                                                Add
+                                            </Button>
+                                        </FormControl>
+                                    )}
+                                />
                             </ListItemSecondaryAction>
                         </ListItem>
                     )}
--- src/Content/console/Faculty.tsx
+++ src/Content/console/Faculty.tsx
@@ -1,13 +1,45 @@
-import { Button } from '@material-ui/core';
-import React, { Fragment, VFC } from 'react';
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Fab,
+    TextField,
+    Tooltip
+} from '@material-ui/core';
+import { Add } from '@material-ui/icons';
+import React, { Fragment, useState, VFC } from 'react';
 import { PopoverItem, TabNav } from '../../Components';
 import { cloneComponent } from '../../utils';
 
 export const Faculty: VFC = () => {
+    const [open, setOpen] = useState(false);
     return (
         <Fragment>
+            <Tooltip title="Add new faculty" arrow>
+                <Box position="fixed" bottom={16} right={16} onClick={() => setOpen(true)}>
+                    <Fab color="primary" style={{ background: '#1976d2' }}>
+                        <Add />
+                    </Fab>
+                </Box>
+            </Tooltip>
+            <Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add new faculty</DialogTitle>
+                <DialogContent>
+                    <TextField autoFocus margin="dense" id="faculty" label="Faculty name" fullWidth />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)} color="primary">
+                        Add
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
             {cloneComponent(10)(
                 <PopoverItem
+                    placement="bottom"
                     renderToggle={(toggle, toggleEl) => (
                         <Button
                             variant="outlined"
