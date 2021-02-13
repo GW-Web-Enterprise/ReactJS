6bf19cb9cd8dde52ed25262add3b116bd97e125e
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-02-13 11:13:18 +0700

    🆙 update: update typings and props for the PopoverItem component

--- src/Components/PopoverItem.tsx
+++ src/Components/PopoverItem.tsx
@@ -1,32 +1,46 @@
-import { Box, Popover } from '@material-ui/core';
-import React, { MutableRefObject, useRef, useState, VFC } from 'react';
+import { Box, Popover, PopoverOrigin } from '@material-ui/core';
+import React, { MutableRefObject, ReactNode, useRef, useState, VFC } from 'react';
 interface PopoverItemProps {
     /** A 'render prop' that renders the JSX element to toggle the Popover */
-    renderToggle: (toggle: () => void, toggleEl: MutableRefObject<null>) => JSX.Element;
+    renderToggle: (toggle: () => void, toggleEl: MutableRefObject<null>) => ReactNode;
     /** A 'render prop' that returns the actual popover content */
-    renderPopContent: () => JSX.Element;
+    renderPopContent: () => ReactNode;
+    placement: 'top' | 'bottom';
 }
 
-export const PopoverItem: VFC<PopoverItemProps> = ({ renderToggle, renderPopContent }) => {
+type PopoverPos = { anchorOrigin: PopoverOrigin; transformOrigin: PopoverOrigin };
+type Placement = 'top' | 'bottom';
+const pos: Record<Placement, PopoverPos> = {
+    top: {
+        anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'center'
+        },
+        transformOrigin: {
+            vertical: 'bottom',
+            horizontal: 'center'
+        }
+    },
+    bottom: {
+        anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'center'
+        },
+        transformOrigin: {
+            vertical: 'top',
+            horizontal: 'center'
+        }
+    }
+};
+
+export const PopoverItem: VFC<PopoverItemProps> = ({ renderToggle, renderPopContent, placement }) => {
     const toggleEl = useRef(null);
     const [open, setOpen] = useState(false);
 
     return (
         <Box m={1} display="inline-block">
             {renderToggle(() => setOpen(!open), toggleEl)}
-            <Popover
-                open={open}
-                anchorEl={toggleEl.current}
-                onClose={() => setOpen(false)}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center'
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center'
-                }}
-            >
+            <Popover open={open} anchorEl={toggleEl.current} onClose={() => setOpen(false)} {...pos[placement]}>
                 <Box style={{ padding: '16px' }}>{renderPopContent()}</Box>
             </Popover>
         </Box>
