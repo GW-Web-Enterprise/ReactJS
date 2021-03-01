78d0975c8e76a09b2ae57c91f5a13ac008ff7e39
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-01 16:26:33 +0700

    🆙 update: make date mng lib's methods available down the tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import '@app/App.css';
 import { CssBaseline } from '@material-ui/core';
 import { ProvideAuth } from '@app/hooks/useAuth';
 import { ApTemplate, ConsoleTemplate } from '@app/Templates';
+import DateFnsUtils from '@date-io/date-fns';
 
 function App() {
     return (
         <React.Fragment>
             <CssBaseline />
             <Router>
-                <ProvideAuth>
-                    <Switch>
-                        <Route exact path="/ap" render={() => <Redirect to="/ap/login" />} />
-                        <Route exact path="/console" render={() => <Redirect to="/console/overview" />} />
-                        <Route path="/ap" component={ApTemplate} />
-                        <Route path="/console" component={ConsoleTemplate} />
-                    </Switch>
-                </ProvideAuth>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <ProvideAuth>
+                        <Switch>
+                            <Route exact path="/ap" render={() => <Redirect to="/ap/login" />} />
+                            <Route exact path="/console" render={() => <Redirect to="/console/overview" />} />
+                            <Route path="/ap" component={ApTemplate} />
+                            <Route path="/console" component={ConsoleTemplate} />
+                        </Switch>
+                    </ProvideAuth>
+                </MuiPickersUtilsProvider>
             </Router>
         </React.Fragment>
     );
