813dd7c1c6c36dccb455d6c2cecea6491f67d11a
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-02 11:19:20 +0700

    🚑 bug(UI): fix bugs in the form to create repo Bugs fixed: - 2nd deadline field is frozen and cannot be changed - 2nd deadline field's value, min and max constraints are not reset when the diaglog is closed -form data is NOT validated on submit

--- src/Content/console/Repo/AddRepo.tsx
+++ src/Content/console/Repo/AddRepo.tsx
@@ -15,6 +15,7 @@ import { InlineIcon } from '@app/Components/InlineIcon';
 import { DateTimePicker, MuiPickersContext } from '@material-ui/pickers';
 import { Controller, useForm } from 'react-hook-form';
 import { useAuth } from '@app/hooks/useAuth';
+import { REPO_DESC_ERR, REPO_NAME_ERR } from '@app/constants/inputErrs';
 
 const ColorButton = withStyles(() => ({
     root: {
@@ -31,30 +32,53 @@ export const AddRepo = () => {
     const utils = useContext(MuiPickersContext);
     const [loading, setLoading] = useState(false);
     const [dialogOpen, setDialogOpen] = useState(false);
+
     const { current: initialDate } = useRef(new Date()); // Not re-initialized on re-render
-    const firstDeadlineMin = utils?.addDays(initialDate, 7); // At least 7 day to CUD
-    const firstDeadlineMax = utils?.addDays(initialDate, 120); // 120 days to CUD at most
+    const firstDeadlineMin = utils?.addDays(initialDate, 7); // Must give students at least 7 day to CUD
+    const firstDeadlineMax = utils?.addDays(initialDate, 120); // 120 days to CUD
     const [firstDeadline, setFirstDeadline] = useState(firstDeadlineMin); // current selected value of 1st deadline
-    const secondDeadlineMin = utils?.addDays(firstDeadline!, 3); // At least 3 days after the 1st deadline to edit
-    const secondDeadlineMax = utils?.addDays(firstDeadline!, 7); // 7 days after the 1st deadline to edit at most
-    const { control } = useForm();
+    const secondDeadlineMin = utils?.addDays(firstDeadline!, 3); // Must give students at least 3 days to edit after the 1st deadline
+    const secondDeadlineMax = utils?.addDays(firstDeadline!, 14); // 7 days to edit
+
+    const handleClose = () => {
+        setFirstDeadline(firstDeadlineMin);
+        setDialogOpen(false);
+    };
+    const { control, handleSubmit, register, errors } = useForm();
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
     return (
         <Fragment>
             <ColorButton variant="contained" onClick={() => setDialogOpen(true)}>
                 <InlineIcon src={repoIcon} />
                 New Repo
             </ColorButton>
-            <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+            <Dialog open={dialogOpen} onClose={handleClose}>
                 <DialogTitle>New repo for faculty articles</DialogTitle>
                 <DialogContent>
                     <Chip
                         avatar={<Avatar src={currentUser?.photoURL!} />}
                         label={`${currentUser?.displayName} (Owner)`}
                     />
-                    <form noValidate autoComplete="off">
-                        <TextField name="name" autoFocus margin="normal" label="Repo name" fullWidth />
-                        {/* <Box color="error.main">{errors.name?.message}</Box> */}
-                        <TextField name="description" margin="normal" label="Short description (optional)" fullWidth />
+                    <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                        <TextField
+                            inputRef={register(REPO_NAME_ERR)}
+                            name="name"
+                            autoFocus
+                            margin="normal"
+                            label="Repo name"
+                            fullWidth
+                        />
+                        <Box color="error.main">{errors.name?.message}</Box>
+                        <TextField
+                            inputRef={register(REPO_DESC_ERR)}
+                            name="description"
+                            margin="normal"
+                            label="Short description (optional)"
+                            fullWidth
+                        />
+                        <Box color="error.main">{errors.description?.message}</Box>
                         <Controller
                             name="First deadline"
                             control={control}
@@ -84,15 +108,15 @@ export const AddRepo = () => {
                             name="Second deadline"
                             control={control}
                             defaultValue={secondDeadlineMin}
-                            render={({ ref, value: _unused, ...rest }) => (
+                            render={({ ref, ...rest }) => (
                                 <DateTimePicker
                                     margin="normal"
                                     variant="dialog"
                                     label="Second deadline"
-                                    value={secondDeadlineMin}
                                     minDate={secondDeadlineMin}
                                     maxDate={secondDeadlineMax}
-                                    helperText="Students cannot edit their articles after the 2nd deadline"
+                                    minDateMessage="Please repick the 2nd deadline"
+                                    maxDateMessage="Please repick the 2nd deadline"
                                     autoOk
                                     disablePast={true}
                                     hideTabs
--- src/constants/inputErrs.ts
+++ src/constants/inputErrs.ts
@@ -1,30 +1,37 @@
-import { REGEX_EMAIL, REGEX_FULLNAME, REGEX_LETTERS_ONLY_NO_LEADING_TRAILING_DOUBLE_SPACES, REGEX_NO_SPACES } from "@app/constants/regexes";
-import { RegisterOptions } from "react-hook-form";
+import {
+    REGEX_EMAIL,
+    REGEX_FULLNAME,
+    REGEX_LETTERS_ONLY_NO_LEADING_TRAILING_DOUBLE_SPACES,
+    REGEX_NO_SPACES
+} from '@app/constants/regexes';
+import { RegisterOptions } from 'react-hook-form';
 
 // 📌 USER
-const fullnameErr = 'Your fullname must contain between 4 and 64 characters'
+const fullnameErr = 'Your fullname must contain between 4 and 64 characters';
 export const FULLNAME_INP_ERR: RegisterOptions = {
     required: fullnameErr,
     minLength: { value: 4, message: fullnameErr },
     maxLength: { value: 64, message: fullnameErr },
-    pattern: { value: REGEX_FULLNAME, message: 'Please enter a valid fullname (alphabet characters only, no space at both end, and no consecutive spaces)' }
-}
-
+    pattern: {
+        value: REGEX_FULLNAME,
+        message:
+            'Please enter a valid fullname (alphabet characters only, no space at both end, and no consecutive spaces)'
+    }
+};
 const emailErr = 'Please enter a valid email';
 export const EMAIL_INP_ERR: RegisterOptions = {
     required: emailErr,
     minLength: { value: 5, message: emailErr },
     maxLength: { value: 50, message: emailErr },
     pattern: { value: REGEX_EMAIL, message: emailErr }
-}
-
+};
 const passwordErr = 'Your password must contain between 4 and 60 characters';
 export const PASSWORD_INP_ERR: RegisterOptions = {
     required: passwordErr,
     minLength: { value: 6, message: passwordErr },
     maxLength: { value: 60, message: passwordErr },
     pattern: { value: REGEX_NO_SPACES, message: 'Password should not contain any whitespace' }
-}
+};
 
 // 📌 FACULTY
 const facultyNameErr = 'Must contain between 3 and 50 characters';
@@ -32,5 +39,18 @@ export const FACULTY_NAME_ERR: RegisterOptions = {
     required: facultyNameErr,
     minLength: { value: 3, message: facultyNameErr },
     maxLength: { value: 50, message: facultyNameErr },
-    pattern: { value: REGEX_LETTERS_ONLY_NO_LEADING_TRAILING_DOUBLE_SPACES, message: 'Only letters and single spaces are accepted. Remove any leading or traling space you might have entered' }
-}
+    pattern: {
+        value: REGEX_LETTERS_ONLY_NO_LEADING_TRAILING_DOUBLE_SPACES,
+        message:
+            'Only letters and single spaces are accepted. Remove any leading or traling space you might have entered'
+    }
+};
+
+// 📌 REPO
+export const REPO_NAME_ERR: RegisterOptions = {
+    required: `Please enter the repo's name`,
+    maxLength: { value: 100, message: `Repo's name is limited to 100 characters` }
+};
+export const REPO_DESC_ERR: RegisterOptions = {
+    maxLength: { value: 100, message: `Description is limited to 100 characters` }
+};
