419a4952a1178245d10a133bac40560f035014fc
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-03 11:56:47 +0700

    💄 update: button to edit and delete each faculty's repo

--- src/Content/console/Repo/RepoList.tsx
+++ src/Content/console/Repo/RepoList.tsx
@@ -1,5 +1,9 @@
 import {
+    IconButton,
     Link,
+    List,
+    ListItem,
+    ListItemText,
     Paper,
     Table,
     TableBody,
@@ -9,43 +13,79 @@ import {
     TableRow,
     Tooltip
 } from '@material-ui/core';
-import React, { VFC } from 'react';
+import React, { useState, VFC } from 'react';
 import firebase from 'firebase/app';
 import { useFirestoreQuery } from '@app/hooks/useFirestoreQuery';
 import { RepoRead } from '@app/typings/schemas';
+import { KeyboardArrowDown, KeyboardArrowUp, MoreVert } from '@material-ui/icons';
+import { PopoverItem } from '@app/Components/PopoverItem';
 
-type Props = { facultyId: string };
-
+type RepoListProps = { facultyId: string };
 const reposRef = firebase.firestore().collection('repos');
-export const RepoList: VFC<Props> = ({ facultyId }) => {
+export const RepoList: VFC<RepoListProps> = ({ facultyId }) => {
     const { data = [], status } = useFirestoreQuery(reposRef.where('facultyId', '==', facultyId));
     return (
         <TableContainer component={Paper}>
             <Table style={{ minWidth: 650 }}>
                 <TableHead>
                     <TableRow>
+                        <TableCell></TableCell>
                         <TableCell>Repo name</TableCell>
                         <TableCell align="right">Close date and time</TableCell>
                         <TableCell align="right">Final date and time</TableCell>
+                        <TableCell></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {status === 'success' &&
-                        data.map(({ id, name, description, closeTimestamp, finalTimestamp }: RepoRead) => (
-                            <TableRow key={id}>
-                                <TableCell component="th" scope="row">
-                                    <Tooltip title={description ?? 'none'} placement="bottom">
-                                        <Link component="button" variant="body2">
-                                            {name}
-                                        </Link>
-                                    </Tooltip>
-                                </TableCell>
-                                <TableCell align="right">{closeTimestamp.toDate().toLocaleString()}</TableCell>
-                                <TableCell align="right">{finalTimestamp.toDate().toLocaleString()}</TableCell>
-                            </TableRow>
-                        ))}
+                        data.map((repoDoc: RepoRead) => <Row key={repoDoc.id} repoDoc={repoDoc} />)}
                 </TableBody>
             </Table>
         </TableContainer>
+    );
+};
+
+type RowProps = { repoDoc: RepoRead };
+const Row: VFC<RowProps> = ({ repoDoc }) => {
+    const [open, setOpen] = useState(false);
+    const { id, name, description, closeTimestamp, finalTimestamp } = repoDoc;
+    return (
+        <TableRow key={id}>
+            <TableCell>
+                <IconButton aria-label="Toggle list of articles" size="small" onClick={() => setOpen(!open)}>
+                    {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                </IconButton>
+            </TableCell>
+            <TableCell component="th" scope="row">
+                <Tooltip title={description ?? ''} placement="bottom">
+                    <Link component="button" variant="body2">
+                        {name}
+                    </Link>
+                </Tooltip>
+            </TableCell>
+            <TableCell align="right">{closeTimestamp.toDate().toLocaleString()}</TableCell>
+            <TableCell align="right">{finalTimestamp.toDate().toLocaleString()}</TableCell>
+            <TableCell>
+                <PopoverItem
+                    placement="bottomRight"
+                    padding={0}
+                    renderToggle={(toggle, toggleEl) => (
+                        <IconButton aria-label="View more options" size="small" onClick={toggle} ref={toggleEl}>
+                            <MoreVert />
+                        </IconButton>
+                    )}
+                    renderPopContent={() => (
+                        <List dense>
+                            <ListItem button>
+                                <ListItemText primary="Edit repo" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText primary="Delete repo" />
+                            </ListItem>
+                        </List>
+                    )}
+                />
+            </TableCell>
+        </TableRow>
     );
 };
