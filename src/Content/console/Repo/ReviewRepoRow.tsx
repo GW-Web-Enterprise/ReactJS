059c19296f4019135a4e7e5b5a32c5a8329ec4bc
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-08 21:24:53 +0700

    🍭 example: a list of dropboxes for the coordinators to review

--- src/Content/console/Repo/ReviewRepoRow.tsx
+++ src/Content/console/Repo/ReviewRepoRow.tsx
@@ -22,19 +22,19 @@ export const ReviewRepoRow: IRepoCollapsibleRow = ({ open }) => {
                 <Collapse in={open} timeout="auto" unmountOnExit>
                     <Box margin={1}>
                         <Typography variant="h6" gutterBottom component="div">
-                            Review files uploaded from students in the faculty
+                            Review dropboxes of students in the faculty
                         </Typography>
                         <Table size="small" aria-label="purchases">
                             <TableHead>
                                 <TableRow>
                                     <TableCell>
-                                        <strong>Filename</strong>
+                                        <strong>Owner's name</strong>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <strong>Owner's email</strong>
                                     </TableCell>
                                     <TableCell align="right">
                                         <strong>Created at</strong>
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <strong>Uploaded by</strong>
                                     </TableCell>
                                     <TableCell align="right">
                                         <strong>Status</strong>
@@ -45,10 +45,12 @@ export const ReviewRepoRow: IRepoCollapsibleRow = ({ open }) => {
                             <TableBody>
                                 <TableRow>
                                     <TableCell component="th" scope="row">
-                                        React Hooks
+                                        Bill Gates
                                     </TableCell>
-                                    <TableCell align="right">{new Date().toLocaleString()}</TableCell>
-                                    <TableCell align="right">Bill Gates</TableCell>
+                                    <TableCell align="right">billgates@gmail.com</TableCell>
+                                    <TableCell align="right">
+                                        {new Date('2021-02-08T14:22:12Z').toLocaleString()}
+                                    </TableCell>
                                     <TableCell align="right">Pending</TableCell>
                                     <TableCell>
                                         <PopoverItem
@@ -68,6 +70,38 @@ export const ReviewRepoRow: IRepoCollapsibleRow = ({ open }) => {
                                                 <List component="nav" dense>
                                                     <ListItem button>Download to view</ListItem>
                                                     <ListItem button>Approve</ListItem>
+                                                    <ListItem button>Reject</ListItem>
+                                                </List>
+                                            )}
+                                        />
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell component="th" scope="row">
+                                        Tim Apple
+                                    </TableCell>
+                                    <TableCell align="right">timapple@gmail.com</TableCell>
+                                    <TableCell align="right">
+                                        {new Date('2021-02-08T14:22:12Z').toLocaleString()}
+                                    </TableCell>
+                                    <TableCell align="right">Approved</TableCell>
+                                    <TableCell>
+                                        <PopoverItem
+                                            placement="bottomRight"
+                                            padding={0}
+                                            renderToggle={(toggle, toggleEl) => (
+                                                <IconButton
+                                                    aria-label="View more options"
+                                                    size="small"
+                                                    onClick={toggle}
+                                                    ref={toggleEl}
+                                                >
+                                                    <MoreVert />
+                                                </IconButton>
+                                            )}
+                                            renderPopContent={close => (
+                                                <List component="nav" dense>
+                                                    <ListItem button>Download to view</ListItem>
                                                     <ListItem button>Reject</ListItem>
                                                 </List>
                                             )}
