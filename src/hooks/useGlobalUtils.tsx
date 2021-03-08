5727dbe1d0fccd4905f0baf866c642743f6616e4
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-08 15:50:38 +0700

    ⭐ new(global utiles): show a loading icon with message

--- src/hooks/useGlobalUtils.tsx
+++ src/hooks/useGlobalUtils.tsx
@@ -1,9 +1,11 @@
-import { Snackbar } from '@material-ui/core';
+import { CircularProgress, Snackbar } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 import { createContext, Dispatch, Fragment, ReactNode, SetStateAction, useContext, useRef, useState, VFC } from 'react';
 
 type IGlobalUtilsContext = {
     showAlert: Dispatch<SetStateAction<AlertInfo>>;
+    /** Show a loading icon with the passed in message. Drop in an empty string '' to turn off the loading */
+    showLoading: Dispatch<SetStateAction<string>>;
 };
 const GlobalUtilsContext = createContext({} as IGlobalUtilsContext);
 
@@ -12,9 +14,10 @@ export const useGlobalUtils = () => useContext(GlobalUtilsContext);
 type AlertInfo = { status: 'success' | 'error'; message: string } | null;
 export const ProvideGlobalUtils: VFC<{ children: ReactNode }> = ({ children }) => {
     const [alertInfo, showAlert] = useState<AlertInfo>(null);
+    const [loadingMsg, showLoading] = useState('');
     // useRef() creates an object that will keep the same reference between renders
     // -> value will alway point to the same reference between rerender
-    const staticMethods = useRef<IGlobalUtilsContext>({ showAlert });
+    const staticMethods = useRef<IGlobalUtilsContext>({ showAlert, showLoading });
     return (
         <Fragment>
             <Snackbar
@@ -26,6 +29,11 @@ export const ProvideGlobalUtils: VFC<{ children: ReactNode }> = ({ children }) =
                 <Alert onClose={() => showAlert(null)} severity={alertInfo?.status}>
                     {alertInfo?.message}
                 </Alert>
+            </Snackbar>
+            <Snackbar open={!!loadingMsg}>
+                <Fragment>
+                    <CircularProgress size={40} /> &nbsp; {loadingMsg}
+                </Fragment>
             </Snackbar>
             <GlobalUtilsContext.Provider value={staticMethods.current}>{children}</GlobalUtilsContext.Provider>
         </Fragment>
