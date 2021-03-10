bae539a9371ab59ced0160a978d720baab67500e
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-10 19:49:11 +0700

    🆙 update: argument types of getFileListSize()

--- src/typings/files.d.ts
+++ src/typings/files.d.ts
@@ -1,21 +1,11 @@
-/**
- * This CustomFile type is developed from the built-in File type
- */
-interface CustomFile {
+/** These are the only file's metadata that we care about */
+type FileMetaData = {
     name: string;
-    /** File size in bytes */
     size: number;
-    /** Unique file type specifiers
-     * @tutorial https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#unique_file_type_specifiers
-     */
-    type: string;
-    /** UTC miliseconds */
-    lastModified: number;
-    /** The actual content of the local file uploaded from users */
-    file: File;
-}
+    /** In ISO 8601 format */
+    timeCreated: string;
+    /** In ISO 8601 format */
+    updated: string;
+};
 
-/** An array version of the built-in FileList object value of input.files. This version makes it much easier to
- * work with a list of files using the Array.prototype.* methods
- */
-export type CustomFileList = Array<CustomFile>;
+export type CustomFileList = Array<FileMetaData>;
--- src/utils/getFileListSize.ts
+++ src/utils/getFileListSize.ts
@@ -1,9 +1,10 @@
 import { CustomFileList } from '@app/typings/files';
 import { displayFileSize } from '@app/utils/displayFileSize';
 
-export const getFileListSize = (fileList: CustomFileList) => {
+export const getFileListSize = (fileList: CustomFileList | FileList | Array<File>) => {
     let nBytes = 0; // the size of each file is always in bytes
-    fileList.forEach(({ size }) => (nBytes += size));
+    // Spread syntax can be used on the built-in FileList: https://stackoverflow.com/a/58508451/8138591
+    [...fileList].forEach(({ size }) => (nBytes += size));
     let sOutput = nBytes + ' bytes';
     sOutput = displayFileSize(nBytes);
     return [sOutput, nBytes] as const;
