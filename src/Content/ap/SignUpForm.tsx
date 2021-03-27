ecfb4c1c5c659b912a5821cb0df1fb0258852bb6
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-27 16:36:20 +0700

    ✨ Merge email/pass account to Google account on signup

--- src/Content/ap/SignUpForm.tsx
+++ src/Content/ap/SignUpForm.tsx
@@ -1,20 +1,58 @@
 import { EmailField } from '@app/Components/EmailField';
 import { FullnameField } from '@app/Components/FullnameField';
 import { PasswordField } from '@app/Components/PasswordField';
+import { useAuth } from '@app/hooks/useAuth';
 import { Box, Button, Link } from '@material-ui/core';
-import React, { useState, VFC } from 'react';
+import { useState, VFC } from 'react';
+import firebase from 'firebase/app';
 import { useForm } from 'react-hook-form';
 import { Link as RouterLink } from 'react-router-dom';
+import { useGlobalUtils } from '@app/hooks/useGlobalUtils';
 
 type Inps = { email: string; password: string; fullname: string };
 
 const SignUpForm: VFC = () => {
     const [loading, setLoading] = useState(false);
+    const { signup, loginWithGoogle } = useAuth();
+    const { showAlert, showActionBar } = useGlobalUtils();
     const { register, handleSubmit, errors } = useForm<Inps>();
-    const onSubmit = (data: Inps) => {
+    const createUser = async ({ email, password }: Inps) => {
         setLoading(true);
-        console.log(data);
+        await signup(email, password);
     };
+    const linkToGoogleAcc = async (email: string, password: string) => {
+        const credential = firebase.auth.EmailAuthProvider.credential(email, password);
+        const { user } = await loginWithGoogle();
+        if (user!.email !== email)
+            return showAlert({
+                status: 'error',
+                message: 'Failed to link account because you login with a different email address'
+            });
+        await user!.linkWithCredential(credential);
+        showAlert({
+            status: 'success',
+            message: 'Account is linked successfully, you can now use this email and password to login'
+        });
+    };
+    const onSubmit = (data: Inps) =>
+        createUser(data).catch(async err => {
+            const { code } = err;
+            const { email, password } = data;
+            setLoading(false);
+            if (code === 'auth/email-already-in-use') {
+                if ((await firebase.auth().fetchSignInMethodsForEmail(email)).length === 2)
+                    return showAlert({
+                        status: 'error',
+                        message: `An account already exists with the e-mail ${email}`
+                    });
+                showActionBar({
+                    message: 'There is already a Google account with this email address',
+                    hint: 'Link account',
+                    actionFunc: () => linkToGoogleAcc(email, password)
+                });
+            }
+            code === 'auth/invalid-email' && showAlert({ status: 'error', message: 'Email is invalid' });
+        });
     return (
         <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
             <FullnameField register={register} />
