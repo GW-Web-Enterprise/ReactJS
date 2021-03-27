1705f479023eed159cd77712bf2d55795fb1bf5e
Son Xuan Nguyen <xuansonnguyen2000@gmail.com>
2021-03-27 16:11:14 +0700

    🍱 add action snackbar

--- src/hooks/useGlobalUtils.tsx
+++ src/hooks/useGlobalUtils.tsx
@@ -6,8 +6,12 @@ import {
     DialogActions,
     DialogContent,
     DialogContentText,
-    DialogTitle
+    DialogTitle,
+    IconButton,
+    Slide,
+    SlideProps
 } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import { Alert } from '@material-ui/lab';
 import { createContext, Dispatch, Fragment, ReactNode, SetStateAction, useContext, useRef, useState, VFC } from 'react';
 
@@ -16,6 +20,7 @@ type IGlobalUtilsContext = {
     /** Show a loading icon with the passed in message. Drop in an empty string '' to turn off the loading */
     showLoading: Dispatch<SetStateAction<string>>;
     showDeleteDialog: Dispatch<SetStateAction<IDeleteDialog>>;
+    showActionBar: Dispatch<SetStateAction<IActionBar>>;
 };
 const GlobalUtilsContext = createContext({} as IGlobalUtilsContext);
 
@@ -30,25 +35,59 @@ type IDeleteDialog = {
     /** Invoked when the Delete Dialog is closed */
     cleanup?: () => void;
 } | null;
+type IActionBar = {
+    message: string;
+    hint: string;
+    actionFunc: () => void;
+} | null;
 export const ProvideGlobalUtils: VFC<{ children: ReactNode }> = ({ children }) => {
     const [alertInfo, showAlert] = useState<AlertInfo>(null);
+    const [actionBarParams, showActionBar] = useState<IActionBar>(null);
     const [loadingMsg, showLoading] = useState('');
     const [deleteDialogParams, showDeleteDialog] = useState<IDeleteDialog>(null);
     // useRef() creates an object that will keep the same reference between renders
     // -> value will alway point to the same reference between rerender
-    const staticMethods = useRef<IGlobalUtilsContext>({ showAlert, showLoading, showDeleteDialog });
+    const staticMethods = useRef<IGlobalUtilsContext>({ showAlert, showLoading, showDeleteDialog, showActionBar });
     return (
         <Fragment>
             <Snackbar
                 anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
                 open={!!alertInfo}
-                autoHideDuration={6000}
+                autoHideDuration={8000}
                 onClose={() => showAlert(null)}
             >
                 <Alert onClose={() => showAlert(null)} severity={alertInfo?.status}>
                     {alertInfo?.message}
                 </Alert>
             </Snackbar>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center'
+                }}
+                open={!!actionBarParams}
+                autoHideDuration={8000}
+                onClose={() => showActionBar(null)}
+                message={actionBarParams?.message}
+                TransitionComponent={(props: Omit<SlideProps, 'direction'>) => <Slide {...props} direction="up" />}
+                action={
+                    <Fragment>
+                        <Button
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                actionBarParams?.actionFunc();
+                                showActionBar(null);
+                            }}
+                        >
+                            <strong>{actionBarParams?.hint}</strong>
+                        </Button>
+                        <IconButton size="small" aria-label="close" color="inherit" onClick={() => showActionBar(null)}>
+                            <Close fontSize="small" />
+                        </IconButton>
+                    </Fragment>
+                }
+            />
             <Snackbar open={!!loadingMsg}>
                 <Fragment>
                     <CircularProgress size={40} /> &nbsp; {loadingMsg}
